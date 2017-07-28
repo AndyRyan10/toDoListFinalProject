c190c217f9bd4a4ff8c5d4e2b196e768fbf03a78
AndyRyan10 <ahryan@gmail.com>
2017-07-28 08:27:07 -0400

    View Controller for Data Entry 7/28

--- toDoAppFinalProject/ListViewController.swift
+++ toDoAppFinalProject/ListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UITableViewController, UIPopoverPresentationControllerDelegate {
 
 // MARK: - Identifier Constants
     
@@ -18,6 +18,16 @@ class ListViewController: UITableViewController {
 
     @IBOutlet var toDoList: UITableView!
     
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "NewToDoViewController")
+        viewController.modalPresentationStyle = .popover
+        let popover: UIPopoverPresentationController = viewController.popoverPresentationController!
+        popover.barButtonItem = sender
+        popover.delegate = self
+        present(viewController, animated: true, completion: nil)
+    }
     
 // MARK: - TableView Controls
 
@@ -51,6 +61,19 @@ class ListViewController: UITableViewController {
     }
     
     
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+        
+        
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+    
+
     
     // MARK: - Properties
     // MARK: - View Controller Life Cycle
@@ -65,16 +88,42 @@ class ListViewController: UITableViewController {
         toDoList.reloadData()
     }
     
-    // MARK: - Navigation
-    // MARK: - IBActions
-    // MARK: - Helper Functions
+// MARK: - Navigation
+// MARK: - IBActions
+// MARK: - Helper Functions
     func imageForPoms(pomodoros: Int) -> UIImage?{
         let imageName = "POM#\(pomodoros).png"
         return UIImage(named: imageName)
     }
     
-    // MARK: - Delegates / Extensions
-
+// MARK: - Delegates / Extensions
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .fullScreen
+    }
+    
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        let navigationController = UINavigationController(rootViewController: controller.presentedViewController)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(ListViewController.dismissViewController))
+        
+        let saveButton = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(ListViewController.saveViewController))
+        navigationController.topViewController?.navigationItem.title = "Add To Do"
+        navigationController.topViewController?.navigationItem.rightBarButtonItem = saveButton
+        navigationController.topViewController?.navigationItem.leftBarButtonItem = cancelButton
+        
+        return navigationController
+    }
+    
+    func dismissViewController() {
+        
+        print("cancel clicked")
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func saveViewController() {
+        print("save clicked")
+        self.dismiss(animated: true, completion: nil)
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -84,18 +133,7 @@ class ListViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+   
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
---
+++ toDoAppFinalProject/NewToDo.swift
@@ -0,0 +1,116 @@
+//
+//  NewToDo.swift
+//  toDoAppFinalProject
+//
+//  Created by Andrew Ryan on 7/27/17.
+//  Copyright © 2017 AndrewRyan. All rights reserved.
+//
+
+import UIKit
+
+class NewToDo: UIViewController {
+    
+// MARK: - Identifier Constants
+    
+    var newTitle: String = ""
+    var dueDate: Date? = nil
+    var newPoms: Int? = nil
+    
+// MARK: - Interface Builder Outlets
+    
+    @IBOutlet weak var userTitle: UITextField!
+    
+    @IBOutlet weak var dueDateLabel: UILabel!
+    
+    @IBOutlet weak var dueDateButton: UIButton!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    
+    @IBAction func dueDateButtonPushed(_ sender: Any) {
+        dueDatePicker.isHidden = false
+        pomSlider.isHidden = true
+        zeroLabel.isHidden = true
+        threeLabel.isHidden = true
+        
+    }
+    @IBAction func dueDateChanged(_ sender: UIDatePicker) {
+        
+        dueDate = dueDatePicker.date
+        let dateForDisplay = dueDate
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dueDateButton.setTitle(String(dateFormatter.string(from: dateForDisplay!)), for: .normal)
+
+    }
+    @IBOutlet weak var pomPickerButton: UIButton!
+    @IBOutlet weak var pomPickerImage: UIImageView!
+    @IBAction func pomSelectorPushed(_ sender: Any) {
+        dueDatePicker.isHidden = true
+        pomSlider.isHidden = false
+        zeroLabel.isHidden = false
+        threeLabel.isHidden = false
+        
+    }
+    @IBOutlet weak var pomSlider: UISlider!
+    
+    @IBAction func pomSliderChanged(_ sender: Any) {
+        
+        if pomSlider.value > 0 {
+            pomPickerButton.setTitle(nil, for: .normal)
+            let poms = Int(pomSlider.value)
+            let imageName = "POM#\(poms).png"
+            pomPickerButton.setImage(UIImage(named: imageName), for: .normal)
+            pomPickerButton.contentMode = .scaleAspectFit
+            
+        } else {
+            
+            pomPickerButton.setTitle("None", for: .normal)
+        }
+
+    }
+    @IBOutlet weak var zeroLabel: UILabel!
+    
+    @IBOutlet weak var threeLabel: UILabel!
+// MARK: - Properties
+// MARK: - View Controller Life Cycle
+// MARK: - Navigation
+// MARK: - IBActions
+// MARK: - Helper Functions
+// MARK: - Delegates / Extensions
+    
+    func imageForPoms(pomodoros: Int) -> UIImage?{
+        let imageName = "POM#\(pomodoros).png"
+        return UIImage(named: imageName)
+    }
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dueDatePicker.isHidden = true
+        pomSlider.isHidden = true
+        zeroLabel.isHidden = true
+        threeLabel.isHidden = true
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
